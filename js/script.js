6400f8364c3e039a3bb93db79aa7dba35716d76f
SpencerLaudie <rsl34@byu.edu>
2022-01-07 01:36:00 -0700

    all new portfolio design

--- js/script.js
+++ js/script.js
@@ -1,47 +1,64 @@
-// JavaScript File
-/* global $ location Typed*/
-// nav bar background color change
-$(document).ready(function() {
-  window.onscroll = function() {myFunction()};
-    var height = .15 * $(window).height();
-    function myFunction() {
-        if (document.body.scrollTop > height || document.documentElement.scrollTop > height) {
-            document.getElementById("nav").className = "addNavBackground";
-        } else {
-            document.getElementById("nav").className = "rmvNavBackground";
-        }
-    }
-    
-    // smooth scroll to anchor
-    $('a[href*="#"]')
-      .click(function(event) {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-              scrollTop: target.offset().top-30
-            }, 1000);
-          }
-        }
-      });
-    
-    /* sidebar */
-    $("#hamburger").click(function (e) {
-        $("#mobileSidebar").animate({width:'toggle'},350);
-        e.stopImmediatePropagation();
-    });
-    
-    $(document).click(function (e) {
-        if($("#mobileSidebar").is(":visible") && !$("#mobileSidebar").is(e.target)) {
-            $("#mobileSidebar").animate({width:'toggle'},350);
-        }
-    });
-        
-    $(window).stellar();
-    
-    //scroll-x
+class Project {
+  constructor(title, description, image="", link="javascript:void(0);,'self") {
+    this.title = title;
+    this.description = description;
+    this.image = image;
+    this.link = link;
+  }
+}
 
+const projects = [
+  new Project("Guava", "An mobile app for creating and sharing travel itineraries.", "/images/guava.png", "https://github.com/SpencerLaudie/Guava"),
+  new Project("","This site is still under development. Come back soon to see more of my projects!", "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="),
+]
+
+var index = 0;
+
+$(document).ready(function() {
+  // populate projects with first four
+  while (index < projects.length && index < 4) {
+    appendProject(projects[index]);
+    index++;
+  }
+  isMore();
+
+  // smooth scroll to anchor
+  $('a[href*="#"]')
+    .click(function(event) {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+        }
+      }
+    });
 });
-  
+
+function appendProject(proj) {
+  console.log(proj.link);
+  let html = "<div class='project-container' onclick=\"window.open('" + proj.link + "');\"><div class='project-card'>";
+  html += "<div class='project-thumb'><img src='" + proj.image + "'/></div>";
+  html += "<div class='project-desc'><h3>" + proj.title + "</h3>";
+  html += "<p>" + proj.description + "</p></div></div></a></div>";
+  $('#projects').append(html);
+}
+
+function seeMore() {
+  let i = 0;
+  while (index < projects.length && i < 2) {
+    appendProject(projects[index]);
+    index++;
+    i++;
+  }
+  isMore();
+}
+
+function isMore() {
+  if (index >= projects.length) {
+    $('#see_more').hide();
+  }
+}
